b649376fc2f48ffba2b4262c332d547e9d885e8d
zayroninfotech <zayroninfotech@gmail.com>
2026-07-11 17:40:51 +0530

    new

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -157,18 +157,6 @@ export default function Layout({ title, actions, children }) {
             {/* Actions */}
             {actions && <div style={{ display: 'flex', gap: 8 }}>{actions}</div>}
 
-            {/* Notification */}
-            <button className="notif-btn">
-              <svg width="18" height="18" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
-                <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/>
-                <path d="M13.73 21a2 2 0 0 1-3.46 0"/>
-              </svg>
-              <span className="notif-dot" />
-            </button>
-
-            {/* Divider */}
-            <div className="topbar-div" />
-
             {/* User pill */}
             <div className="user-pill">
               <div className="user-pill-avatar">{initials}</div>
